28b06e9ccd5736cf10012f30cb5bc0225b8ca7cb
Kristiaan De Jongh <kristiaan@dejongh.de>
2021-01-08 15:03:37 +0100

    First version README

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# GettingAndCleaningDataCourseProject
-Gettings and Cleaning Data Course Project of Courseras Data Science
+# Getting and Cleaning Data Course Project
+
+## Intro
+
+This project contains one R script named 'run_analysis.R' that will download the 
+[Human Activity Recognition Using Smartphones Data Set](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones), 
+containing the recordings of 30 subjects performing activities of daily living (ADL) 
+while carrying a waist-mounted smartphone with embedded inertial sensors.
+
+The data set is then cleaned and a new data set is created containing the average of 
+every variable that is a mean or standard deviation calculation of a measurement. 
+
+## Usage
+
+The R script can be executed by sourcing the file:
+
+```{r eval=FALSE}
+source('./run_analysis.R')
+```
+
+It takes no arguments and needs any configuration.
+
+The R script will:
+
+1. Download the data set from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+and unzip the content to the current working directory.
+
+2. Load some common data to the training and test sets, i.e. the activity labels ('activity_labels.txt') 
+and the feature names ('features.txt').
+
+3. Load the test data set files ('subject_test.txt', 'y_test.txt', 'X_test.txt')
+and merge them into one data frame.
+
+4. Load the training data set files ('subject_train.txt', 'y_train.txt', 'X_train.txt')
+and merge them into one data frame.
+
+5. Merge the test and training data frames, thereby selecting only the columns that
+contain an average or standard deviation calculation.
+
+6. Rename these column names to a more friendly name and write out the resulting data frame ('tidy_data.txt').
+
+7. To compute the average of each variable of the tidy data frame for each activity and subject, 
+a new data frame is created by melting the tidy data (all columns except for 'subject' and 'activity').
+The result is a data frame with 4 columns 'subject', 'activity', 'feature', 'value' with 'feature' being
+the name of the column in the tidy data set and 'value' its measurement:
+
+> # A tibble: 679,734 x 4
+>   subject activity feature                      value
+>     <int> <chr>    <fct>                        <dbl>
+> 1       2 WALKING  Time.BodyAcceleration.mean.X 0.257
+> 2       2 WALKING  Time.BodyAcceleration.mean.X 0.286
+> 3       2 WALKING  Time.BodyAcceleration.mean.X 0.275
+> ...
+
+8. Finally this data set is aggregated by calculating the mean of the measurements 
+grouped by 'subject', 'activity' and 'feature' and the resulting data frame is 
+written to the file 'average_data.txt'.
--- run_analysis.R
+++ run_analysis.R
@@ -2,37 +2,38 @@ library(dplyr)
 library(data.table)
 library(stringr)
 
-## Download and unzip data set
+## 1. Download and unzip data set
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
               destfile = "UCI_HAR_Dataset.zip", method = "curl")
 unzip("UCI_HAR_Dataset.zip")
 
-# Load the common master data
-# 1.1 load the activity labels
+## 2. Load the common master data
+## 2.1 load the activity labels
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt", sep = " ", col.names = c("key", "activity"))
 
-# 1.2 load the feature names
+## 2.2 load the feature names
 featureNames <- read.table("UCI HAR Dataset/features.txt", sep = " ", col.names = c("key", "feature"))
 
-# 2. Load test data
+## 3. Load test data
 testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = c("subject"))
 testLabels <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = c("key"))
 testLabels <- merge(testLabels, activityLabels, by.x = "key", by.y = "key")
 testSet <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = featureNames$feature)
 testSet <- as_tibble(cbind(testSubjects, testLabels, testSet))
 
-# 3. Load training data
+## 4. Load training data
 trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = c("subject"))
 trainLabels <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = c("key"))
 trainLabels <- merge(trainLabels, activityLabels, by.x = "key", by.y = "key")
 trainSet <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = featureNames$feature)
 trainSet <- as_tibble(cbind(trainSubjects, trainLabels, trainSet))
 
-# merge the 2 data sets and select all the columns that have ".mean." or ".std." 
-# in their name (+ "subject" and "activity")
+## 5. Merge the 2 data sets
 fullSet <- rbind(testSet, trainSet)
+# 5.1 select only the columns that have ".mean." or ".std." in their name (+ "subject" and "activity")
 tidySet <- select(fullSet, grep("subject|activity|\\.mean\\.|\\.std\\.", names(fullSet), value = TRUE)) %>%
     arrange("subject", "activity") %>% # sort by subject and activity
+# 5.2 rename those columns
     rename_with(function(name) { # user friendly name of the variable
         str_replace_all(
             str_replace_all(
@@ -48,13 +49,19 @@ tidySet <- select(fullSet, grep("subject|activity|\\.mean\\.|\\.std\\.", names(f
                 "tGravity", "Time.Gravity"), 
             "fGravity", "Freq.Gravity")
     }) 
+# 5.3 write out the tidy data set
 write.table(tidySet, file = "tidy_data.txt", row.name = FALSE, quote = FALSE) 
 
-# melt the data by adding a column 'feature' with value the feature name
+## 6. Create a new data set with the average values of each feature
+# 6.1 melt the data by adding a column 'feature' and the feature name as value
 tidySet2 <- as_tibble(melt(as.data.table(tidySet), id.vars = c("subject", "activity"), variable.name = "feature"))
-# compute the average by grouping by subject, activity and feature
+
+# 6.2 compute the average by grouping by subject, activity and feature
 avgValuesSet <- mutate(tidySet2, subject = as.factor(subject), activity = as.factor(activity)) %>% 
     group_by(subject, activity, feature) %>%
     summarize(average = mean(value))
 print(avgValuesSet)
+# 6.3 write out the new data set
 write.table(avgValuesSet, file = "average_data.txt", row.name = FALSE, quote = FALSE) 
+
+print("Done.")
